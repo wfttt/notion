10f2b39724d95db9616cdca5afa32f0df8152234
wfttt <51032493+wfttt@users.noreply.github.com>
2024-06-07 21:48:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,65 +1,63 @@
-import { siteConfig } from './lib/site-config'
+import { siteConfig } from './lib/site-config' // 导入 siteConfig 函数
 
 export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // 站点的根 Notion 页面 (必需)
+  rootNotionPageId: 'eff6e710bb4f4cb5b4266e16b0367c89',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+  // 如果你想限制页面到一个 Notion 工作区 (可选)
+  // (这应该是一个 Notion ID; 查看文档以了解如何提取此 ID)
   rootNotionSpaceId: null,
 
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  // 基本站点信息 (必需)
+  name: 'Wiki.8Hedia', // 站点名称
+  domain: 'wiki.8h.ink', // 站点域名
+  author: '8Hedia', // 作者名
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // Open Graph 元数据 (可选)
+  description: '私人分享，禁止外传', // 站点描述
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // 社交账号用户名 (可选)
+  twitter: 'transitive_bs', // Twitter 用户名
+  github: 'transitive-bullshit', // GitHub 用户名
+  linkedin: 'fisch2', // LinkedIn 用户名
+  // mastodon: '#', // 可选 mastodon 个人资料 URL，用于链接验证
+  // newsletter: '#', // 可选的 newsletter URL
+  // youtube: '#', // 可选的 YouTube 频道名称或 `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+  // 默认 Notion 图标和封面图片，以保持站点的一致性 (可选)
+  // 页面特定的值将覆盖这些站点默认设置
+  defaultPageIcon: null, // 默认页面图标
+  defaultPageCover: null, // 默认页面封面图片
+  defaultPageCoverPosition: 0.5, // 默认封面图片位置
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  // 是否启用低质量图像预览 (LQIP) 支持 (可选)
+  isPreviewImageSupportEnabled: true, // 是否启用 LQIP 预览图片支持
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  // 是否启用 Redis 以缓存生成的预览图片 (可选)
+  // 注意：如果启用 Redis，需要设置 `REDIS_HOST` 和 `REDIS_PASSWORD` 环境变量。详见 README
+  isRedisEnabled: false, // 是否启用 Redis 缓存
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
+  // Notion 页面 ID 到 URL 路径的映射 (可选)
+  // 这里定义的任何页面将覆盖其默认 URL 路径
+  // 例如:
   //
   // pageUrlOverrides: {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: null, // 页面 URL 重写
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
+  // 是否使用默认的 Notion 导航风格或自定义导航风格。要使用 `navigationLinks`，需将 `navigationStyle` 设置为 `custom`
+  navigationStyle: 'default' // 导航风格设置为默认
+  // navigationStyle: 'custom', // 导航风格设置为自定义（被注释）
+  // navigationLinks: [ // 自定义导航链接（被注释）
   //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //     title: 'About', // 链接标题
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a' // 页面 ID
   //   },
   //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //     title: 'Contact', // 链接标题
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1' // 页面 ID
   //   }
   // ]
 })
